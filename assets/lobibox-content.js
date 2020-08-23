ea7abc93b058ae494148598398a9f6767993adec
loto14sempre <70084854+loto14sempre@users.noreply.github.com>
2020-08-23 01:22:56 -0300

    Add files via upload

---
+++ assets/lobibox-content.js
@@ -0,0 +1,154 @@
+var IMG_PREFIX = 'assets/';
+//LobiBox default options
+Lobibox.notify.DEFAULTS = $.extend({}, Lobibox.notify.DEFAULTS, {
+    sound: false,
+    position: 'bottom left',
+    title: false,
+    delay: 5000,
+    messageHeight: 90,
+});
+(function() {
+    setTimeout(function() {
+		
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Gabriela da Costa</b> acabou de comprar uma licença'
+        });
+		
+    }, 7000);
+
+    setTimeout(function() {
+		
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>6 pessoas compraram nos últimos 15 minutos.</b>  <br><br><b>Clique</b> e garanta também o seu...'
+        });
+    }, 15000);
+
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Eduardo Silveira</b> acabou de comprar uma licença!'
+        });
+    }, 23000);
+
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>4 pessoas acabaram comprar</b>.<br><br><b>Clique</b> e garanta o também o seu...'
+        });
+    }, 31000);
+
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Tatiane Silva</b> acabou de comprar uma licença'
+        });
+    }, 39000);
+
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Kauê Baptista</b> acabou de comprar comprar uma licença'
+        });
+    }, 47000);
+
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Antonio Moura</b> acabou de comprar uma licença'
+        });
+    }, 55000);
+
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>2 pessoas acabaram de comprar </b>.<br><br><b>Clique</b> e garanta o seu...'
+        });
+    }, 62000);
+
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Alice Silvana da Luz</b> acabou de comprar uma licença'
+        });
+    }, 70000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Rafael Raimundo</b> acabou de comprar comprar uma licença'
+        });
+    }, 78000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Novas 5 pessoas compraram licença nos últimos 10 minutos</b>.<br><br><b>Clique</b> e garanta também o seu...'
+        });
+    }, 86000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Liz Aragão</b> acabou de comprar comprar uma licença'
+        });
+    }, 94000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Novas 6 pessoas compraram nos últimos 15 minutos.'
+        });
+    }, 102000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Nathan de Jesus</b> acabou de comprar uma licença'
+        });
+    }, 110000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>4 pessoas acabaram comprar '
+        });
+    }, 118000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Erick Duarte</b> acabou de comprar uma licença'
+        });
+    }, 126000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Henrique Galvão</b> acabou de comprar comprar uma licença'
+        });
+    }, 134000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Tomás Moraes</b> acabou de comprar comprar uma licença'
+        });
+    }, 142000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>2 pessoas acabaram de comprar </b>.<br><br><b>Clique</b> e garanta o seu...'
+        });
+    }, 150000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Márcio Assunção</b> acabou de comprar uma licença'
+        });
+    }, 158000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Lucas Bruno</b> acabou de comprar uma licença'
+        });
+    }, 166000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Novas 5 pessoas compraram  nos últimos 10 minutos</b>.<br><br><b>Clique</b> e garanta também o seu...'
+        });
+    }, 174000);
+})();
